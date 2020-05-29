a54d942c6a872c7bd9140687a8fa8249a0edb433
Felix <felix.leeb@hotmail.com>
2020-05-29 15:57:28 +0200

    package setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+from omnifig import _lib_info as info
+
+with open('README.md', 'r') as f:
+      lines = f.readlines()
+
+readme = []
+valid = False
+for line in lines:
+      if valid:
+            if 'end-setup-marker-do-not-remove' in line:
+                  valid = False
+            else:
+                  readme.append(line)
+      elif 'setup-marker-do-not-remove' in line:
+            valid = True
+
+README = '\n'.join(readme)
+
+setup(name=info.name,
+      version=info.version,
+      description=info.description,
+      long_description=README,
+      url=info.url,
+      author=info.author,
+      author_email=info.author_email,
+      license=info.license,
+      packages=info.packages,
+      install_requires=info.install_requires,
+      zip_safe=False)
