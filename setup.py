7177c9a79146f2ae7d31bc06e77a394a30e81fea
felixludos <felixludos.info@gmail.com>
2021-02-08 13:41:16 +0100

    version update

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 info = {'__file__':os.path.join(os.path.abspath(os.path.dirname(__file__)), 'omnifig', '_info.py')}
 with open(info['__file__'], 'r') as f:
@@ -33,7 +33,7 @@ setup(name=info.get('name', None),
       author=info.get('author', None),
       author_email=info.get('author_email', None),
       license=info.get('license', None),
-      packages=info.get('packages', [info['name']]),
+      packages=find_packages(),
       entry_points=info.get('entry_points', {}),
       install_requires=info.get('install_requires', []),
       zip_safe=info.get('zip_safe', False),
