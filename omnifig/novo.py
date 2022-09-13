0b682c3442fc97b51ebcf8f4290142f9c8788424
felixludos <felix.leeb@hotmail.com>
2022-09-13 17:31:01 +0200

    prep integration

--- omnifig/novo.py
+++ omnifig/novo.py
@@ -1,5 +1,7 @@
 from typing import List, Dict, Tuple, Optional, Union, Any, Hashable, Sequence, Callable, Generator, Type, Iterable, Iterator
-from omnibelt import unspecified_argument, Singleton
+from collections import OrderedDict
+from omnibelt import unspecified_argument, Singleton, extract_function_signature
+from inspect import Parameter
 from omnibelt.nodes import AutoTreeNode, AutoTreeSparseNode, AutoTreeDenseNode,  AutoAddressNode, AddressNode
 
 
@@ -90,7 +92,6 @@ class SimpleConfigNode(AutoTreeNode):
 	def search(self, *queries, default: Optional[Any] = unspecified_argument, **kwargs):
 		return self.Search(origin=self, queries=queries, default=default, **kwargs)
 
-
 	def peek(self, *queries, default: Optional[Any] = unspecified_argument, **kwargs) -> 'SimpleConfigNode':
 		return self.search(*queries, default=default, **kwargs).find_node()
 
@@ -119,9 +120,78 @@ class SimpleConfigNode(AutoTreeNode):
 		return self.pull(addr, **kwargs)
 
 
+class Configurable:
+	_my_config = None
+
+	@property
+	def my_config(self):
+		return self._my_config
+
+	@classmethod
+	def init_from_spec(cls, spec, args: Tuple, kwargs: Dict[str, Any], silent: Optional[bool] = None):
+		# obj = cls.__new__(*args, **kwargs)
+		# obj._my_config = spec
+		# obj.__init__(*args, **kwargs)
+		# return obj
+		# return cls(*args, **kwargs)
+		cls._my_config = spec
+		with spec.silence(silent):
+			obj = cls(*args, **kwargs)
+		del cls._my_config
+		obj._my_config = spec
+		return obj
+
 
 primitive = (int, float, str, bool, type(None))
 Primitive = Union[primitive]
+
+import io
+import yaml
+from pathlib import Path
+from omnibelt import Path_Registry
+
+class ConfigManager:
+
+	_config_path_delimiter = '/'
+
+	def __init__(self, project):
+		self.registry = Path_Registry()
+		self.project = project
+
+	def register(self, name: str, path: Path, **kwargs):
+		self.registry.new(name, path, **kwargs)
+
+	def auto_register_directory(self, root: Path, exts=('yaml', 'yml')):
+		for ext in exts:
+			for path in root.glob(f'**/*.{ext}'):
+				terms = path.relative_to(root).parts[:-1]
+				name = path.stem
+				ident = self._config_path_delimiter.join(terms + (name,))
+				self.register(ident, path)
+
+
+	def _parse_raw_arg(self, arg):
+		return yaml.safe_load(io.StringIO(arg))
+		# try:
+		# 	if isinstance(mode, str):
+		# 		if mode == 'yaml':
+		# 			return yaml.safe_load(io.StringIO(arg))
+		# 		elif mode == 'python':
+		# 			return eval(arg, {}, {})
+		# 		else:
+		# 			pass
+		# 	else:
+		# 		return mode(arg)
+		# except:
+		# 	pass
+		# return arg
+
+	def parse_argv(self, argv):
+
+
+
+
+		pass
 
 
 class ConfigNode(SimpleConfigNode):
@@ -173,18 +243,32 @@ class ConfigNode(SimpleConfigNode):
 				return self.suffix_fmt.format(suffix=suffix)
 
 
-		def _present_component(self, key, search):
-			cmpn_type = search.component_type
-			mod_types = search.mod_types
-			mod_info = f' (mods=[{", ".join(mod_types)}])' if len(mod_types) > 1 else f' (mod={mod_types[0]})'
-			return f'{key} type={cmpn_type}{mod_info}'
+		def component_creation(self, node, key, cmpn, mods, silent=None):
+			if silent is None:
+				silent = self.silent
+			mod_info = f' (mods=[{", ".join(mods)}])' if len(mods) > 1 else f' (mod={mods[0]})'
+			key = '' if key is None else key + ' '
+			indent = self._node_depth(node) * self.indent
+			return self.log(f'{self.flair}{indent}CREATING {key}type={cmpn}{mod_info}', silent=silent)
+
+
+		def get_key(self, search: 'ConfigNode.Search') -> str:
+
+			queries = search.query_chain
+
+			if len(search.parent_search):
+				queries = queries.copy()
+				queries[0] = f'({queries[0]})' #if getattr(search.parent_search[0]
+
+			if len(queries) > self.max_num_aliases:
+				key = self.transfer.join([queries[0], '...', queries[-1]])
+			else:
+				key = self.transfer.join(queries)
+			return key
 
 
 		def _present_payload(self, search):
-			if len(search.query_chain) > self.max_num_aliases:
-				key = self.transfer.join([search.query_chain[0], '...', search.query_chain[-1]])
-			else:
-				key = self.transfer.join(search.query_chain)
+			key = self.get_key(search)
 			
 			if search.action == 'primitive':
 				value = repr(search.result_node)
@@ -199,11 +283,11 @@ class ConfigNode(SimpleConfigNode):
 				# return f'{key} has {N} {x}:'
 				return f'{key} [{t} with {N} {x}]'
 				
-			elif search.action == 'component':
-				return self._present_component(key, search)
+			# elif search.action == 'component':
+			# 	return self._present_component(key, search)
 
-			elif search.action == 'storage':
-				return self._present_component(key, search)
+			# elif search.action == 'storage':
+			# 	return self._present_component(key, search)
 
 			raise ValueError(f'Unknown action: {search.action!r}')
 
@@ -217,6 +301,23 @@ class ConfigNode(SimpleConfigNode):
 
 			line = f'{self.flair}{indent}{prefix}{result}{suffix}'
 			return self.log(line)
+
+
+		class Silencer:
+			def __init__(self, reporter, silent=True):
+				self.reporter = reporter
+				self.silent = reporter.silent
+				reporter.silent = silent
+
+			def __enter__(self):
+				return self
+
+			def __exit__(self, exc_type, exc_val, exc_tb):
+				self.reporter.silent = self.silent
+
+
+		def silence(self, silent=True):
+			return self.Silencer(self, silent=silent)
 			
 			
 	class Search(SimpleConfigNode.Search):
@@ -228,10 +329,6 @@ class ConfigNode(SimpleConfigNode):
 			self._ask_parent = ask_parent
 			self.lazy_iterator = lazy_iterator
 			self.parent_search = parent_search
-
-		# class SearchFailed(KeyError):
-		# 	def __init__(self, queries):
-		# 		super().__init__(', '.join(queries))
 
 		_confidential_prefix = '_'
 
@@ -270,7 +367,6 @@ class ConfigNode(SimpleConfigNode):
 			self.query_node = node
 			self.result_node = self.process_node(node)
 			return self.result_node
-		
 
 		# _weak_storage_prefix = '<?>'
 		# _strong_storage_prefix = '<!>'
@@ -290,45 +386,55 @@ class ConfigNode(SimpleConfigNode):
 						out = self._resolve_query(self.origin, ref)
 						return self.process_node(out)
 			return node
-		
+
+
 		def package_payload(self, node: 'ConfigNode'): # creates components
-			
 			if node.has_payload:
-				return node.payload
+				payload = node.payload
+				self.action = 'primitive'
+				return payload
 			
 			# complex object - either component or list/dict
-			
-			typ = node.pull('_type', None, silent=True)
-			if typ is None:
+			try:
+				cmpn, mods = node._extract_component_info()
+			except node.NoComponentFound:
 				# check for iterator
 				if self.lazy_iterator is not None:
+					self.action = 'iterator'
+					node.reporter.search_report(self)
 					return self.lazy_iterator(node)
-				
+
+				self.action = 'no-payload'
+
 				# list or dict
 				if isinstance(node, node.SparseNode):
-					product = {}
-					
+					self.product_type = 'dict'
+					product = OrderedDict()
 					for key, child in node.children():
 						product[key] = self.sub_search(node, key)
-					
+
 				elif isinstance(node, node.DenseNode):
+					self.product_type = 'list'
 					product = []
-					
 					for key, child in node.children():
 						product.append(self.sub_search(node, key))
-					
+
 				else:
 					raise TypeError(f'Unknown node type: {node!r}')
-				
-			# create component
-			
-			
-			
-			
-			pass
+
+				return product
+			else:
+				# create component
+				return node._create_component(cmpn, mods, record_key=node.reporter.get_key(self))
+
+
+		def silence(self, silent=True):
+			return self.reporter.silence(silent)
+
 
 		def sub_search(self, node, key):
 			return self.__class__(node, (key,), parent_search=(*self.parent_search, self)).evaluate()
+
 
 		def evaluate(self):
 			try:
@@ -340,79 +446,107 @@ class ConfigNode(SimpleConfigNode):
 			else:
 				self.product = self.package_payload(node)
 				return self.product
-		
-		_ask_parent = True
-		_volatile_prefix = '__'
 
-		def package(self, node):
-			if node is None:
-				return self.default
-			
-			if node.has_payload:
-				payload = node.payload
-				
-				if isinstance(payload, primitive):
-					if isinstance(payload, str) and payload.startswith(self.reference_prefix):
-						ref = payload[len(self.reference_prefix):]
-						
-				
-				else:
-					return payload
-				
-				return payload
-			
-			
-			
-			return self.result_node.payload
+		# _ask_parent = True
+		# _volatile_prefix = '__'
 
 
-		# def _search_path(self, query, path=()):
-		# 	if query in self.origin:
-		# 		return path + (query,)
-		# 	for key, value in self.origin.items():
-		# 		if isinstance(value, SimpleConfigNode):
-		# 			try:
-		# 				return value._search_path(query, path=path + (key,))
-		# 			except self.QueryFailed:
-		# 				pass
-		# 	raise self.QueryFailed(query)
-		#
-		#
-		# def evaluate(self, silent=None):
-		# 	for query in queries:
-		# 		try:
-		# 			path = self._search_path(query, path=())
-		# 		except self.QueryFailed:
-		# 			path = None
-		# 		else:
-		# 			path = self._clean_up_search_path(path)
-		# 			break
-		#
-		# 	return self.package(path, default=default, silent=silent)
+	# class Editor:
+	# 	def __init__(self, readonly=False, **kwargs):
+	# 		super().__init__(**kwargs)
+	# 		self._readonly = readonly
+	#
+	# 	@property
+	# 	def readonly(self):
+	# 		return self._readonly
+	# 	@readonly.setter
+	# 	def readonly(self, value):
+	# 		self._readonly = value
+	#
+	#
+	# def __init__(self, *args, reporter=None, editor=None, **kwargs):
+	# 	if reporter is None:
+	# 		reporter = self.Reporter()
+	# 	if editor is None:
+	# 		editor = self.Editor()
+	# 	super().__init__(*args, **kwargs)
+	# 	del self._readonly
+	# 	self.reporter = reporter
+	# 	self.editor = editor
 
-
-	class Editor:
-		def __init__(self, readonly=False, **kwargs):
-			super().__init__(**kwargs)
-			self._readonly = readonly
-		
-		@property
-		def readonly(self):
-			return self._readonly
-		@readonly.setter
-		def readonly(self, value):
-			self._readonly = value
-		
-
-	def __init__(self, *args, reporter=None, editor=None, **kwargs):
+	def __init__(self, *args, project=None, reporter=None, **kwargs):
 		if reporter is None:
 			reporter = self.Reporter()
-		if editor is None:
-			editor = self.Editor()
 		super().__init__(*args, **kwargs)
-		del self._readonly
 		self.reporter = reporter
-		self.editor = editor
+		self._project = project
+
+	_component_type_key = '_type'
+	_component_mod_key = '_mod'
+
+	class UnknownComponentType(ValueError): pass
+
+	class NoComponentFound(ValueError):pass
+	def _extract_component_info(self) -> Tuple[str, Sequence[str]]:
+		cmpn = self.pull(self._component_type_key, None, silent=True)
+		if cmpn is None:
+			raise self.NoComponentFound
+
+		mods = self.pull(self._component_mod_key, None, silent=True)
+		if mods is None:
+			mods = []
+		elif isinstance(mods, dict):
+			mods = [mod for mod, _ in sorted(mods.items(), key=lambda x: (x[1], x[0]))]
+		elif isinstance(mods, str):
+			mods = [mods]
+		else:
+			raise ValueError(f'Invalid modifier: {mods!r}')
+		return cmpn, mods
+
+	def _fix_args_and_kwargs(self, fn, args, kwargs, *, silent=None):
+		def default_fn(key, default=Parameter.empty):
+			if default is Parameter.empty:
+				default = unspecified_argument
+			return self.pull(key, default, silent=silent)
+		return extract_function_signature(fn, args, kwargs, default_fn=default_fn)
+		if len(args) == 1 and isinstance(args[0], dict):
+			kwargs = args[0]
+			args = ()
+		return args, kwargs
+
+	def _create_component(self, component_type: str, mod_types: Sequence[str],
+	                      args: Tuple = (), kwargs: Dict[str, Any] = None, *,
+	                      record_key: Optional[str] = None, silent: Optional[bool] = None):
+		if kwargs is None:
+			kwargs = {}
+
+		self.reporter.component_creation(self, record_key, component_type, mod_types, silent=silent)
+
+		project = self.project
+		cmpn = project.find_component(component_type)
+		mods = [project.find_modifier(mod) for mod in mod_types]
+
+		cls = cmpn
+		if len(mods):
+			bases = (*reversed(mods), cmpn)
+			cls = type('_'.join(base.__name__ for base in bases), bases, {})
+
+		if issubclass(cls, Configurable):
+			return cls.init_from_spec(self, args, kwargs, silent=silent)
+		fixed_args, fixed_kwargs = self._fix_args_and_kwargs(cls.__init__, args, kwargs, silent=silent)
+		return cls(*fixed_args, **fixed_kwargs)
+
+	def create(self, *args, **kwargs):
+		return self._create_component(*self._extract_component_info(), args=args, kwargs=kwargs)
+
+
+	@property
+	def project(self):
+		if self._project is None:
+			if not self.has_parent:
+				raise ValueError('No project found')
+			return self.parent.project
+		return self._project
 
 	@property
 	def silent(self):
@@ -421,21 +555,21 @@ class ConfigNode(SimpleConfigNode):
 	def silent(self, value):
 		self.reporter.silent = value
 
-	@property
-	def readonly(self):
-		return self.editor.readonly
-	@readonly.setter
-	def readonly(self, value):
-		self.editor.readonly = value
+	# @property
+	# def readonly(self):
+	# 	return self.editor.readonly
+	# @readonly.setter
+	# def readonly(self, value):
+	# 	self.editor.readonly = value
 
 
-	def set(self, addr: str, value: Any, editor=None, reporter=None, **kwargs) -> 'ConfigNode':
-		if editor is None:
-			editor = self.editor
+	def set(self, addr: str, value: Any, reporter=None, **kwargs) -> 'ConfigNode':
+		# if editor is None:
+		# 	editor = self.editor
 		if reporter is None:
 			reporter = self.reporter
 		node, key = self._evaluate_address(addr)
-		return super(AddressNode, node).set(key, value, editor=editor, reporter=reporter, **kwargs)
+		return super(AddressNode, node).set(key, value, reporter=reporter, **kwargs)
 
 
 
@@ -475,11 +609,16 @@ ConfigNode.SparseNode = AutoTreeSparseNode
 ConfigNode.DenseNode = AutoTreeDenseNode
 
 
+from .organization import Project
 
+class NovoProject(Project, name='novo'):
+	ConfigManager = ConfigManager
 
+	def __init__(self, profile=None, **kwargs):
+		super().__init__(**kwargs)
+		self.config_manager = ConfigManager(self)
 
-
-
+	pass
 
 
 
